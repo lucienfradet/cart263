5f7e8b8d8028c012602aaffd0c9337c9127235b5
Lucien C.Fradet <lucien.fradet@hotmail.com>
2022-01-20 16:33:53 -0500

    Ex1: Started working on exercice 1

---
+++ Exercices/wheres-sausage-dog-new-game-plus/js/script.js
@@ -0,0 +1,31 @@
+/**
+Where's Waldo type game
+
+Lucien Cusson-Fradet
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
