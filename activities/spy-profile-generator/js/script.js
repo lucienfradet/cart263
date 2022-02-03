1847b81e17b973970db7f944b39f0f13db4336de
Lucienfradet <90287745+Lucienfradet@users.noreply.github.com>
2022-02-03 14:50:54 -0500

    Started work on activty: Spy Generator!

---
+++ activities/spy-profile-generator/js/script.js
@@ -0,0 +1,30 @@
+/**
+Spy profile generator activity
+Lucien Cusson-Fradet
+*/
+
+"use strict";
+
+
+/**
+
+*/
+function preload() {
+
+}
+
+
+/**
+
+*/
+function setup() {
+
+}
+
+
+/**
+
+*/
+function draw() {
+
+}
