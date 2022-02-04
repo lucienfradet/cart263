f501fe0d062aee5032ffc1018f73be020091c118
Lucien C.Fradet <lucien.fradet@hotmail.com>
2022-02-03 23:44:14 -0500

    Ex3: Started work on the spy exercice!

---
+++ Exercices/spy-profile-generator-plus/js/Initialisation.js
@@ -0,0 +1,85 @@
+class Initialisation extends State {
+  constructor() {
+    super();
+    this.usersName = '';
+  }
+
+  update() {
+
+  }
+
+  display() {
+    background(0);
+    // Prompt
+    push();
+    fill(255);
+    textFont(font.yoster);
+    textSize(23);
+    textAlign(CENTER, CENTER);
+    text(`Entrez votre nom:`, width/2, height/2 - height/16);
+    pop();
+
+    // A line under their text entry
+    push();
+    stroke(150);
+    line(width/2 - width/4, height/2 + height/16, width/2 + width/4, height/2 + height/16);
+    pop();
+
+    // The current name data they're typing in
+    push();
+    fill(255);
+    textFont(font.yoster);
+    textSize(48);
+    textAlign(CENTER, BOTTOM);
+    text(this.usersName, width/2, height/2 + height/16);
+    pop();
+  }
+
+  keyTyped() {
+      this.usersName += key;
+  }
+
+  keyPressed() {
+    if (keyCode === BACKSPACE) {
+      // This is a way to remove the last character in a string!
+      this.usersName = this.usersName.slice(0, this.usersName.length - 1);
+    }
+    else if (keyCode === ENTER) {
+      this.generateSpyProfile();
+      state = new Game();
+    }
+  }
+
+  generateSpyProfile() {
+    let aliasPart1 = random(objectsData.objects);
+    let aliasPart2 = random(scientistData.scientists);
+
+    spyProfile = {
+      realName: this.usersName,
+      alias: aliasPart1 + ' ' + aliasPart2,
+      secretWeapon: random(Object.keys(religionData)),
+      password: this.setPassword()
+    }
+    spyProfile.weaponDescription = religionData[spyProfile.secretWeapon];
+    this.storeData(spyProfile);
+  }
+
+  storeData(data) {
+    let dataString = JSON.stringify(data);
+    localStorage.setItem(DATA_NAME, dataString);
+  }
+
+  setPassword() {
+    while (true) {
+      let password = prompt("Entrez un nouveau mot de passe");
+      let passwordConfirmation = prompt("Confirmez votre mot de passe");
+
+      if (password === passwordConfirmation) {
+        return password;
+      }
+      else {
+        alert('Les mots de passes ne correspondent pas!')
+      }
+    }
+  }
+}
---
+++ Exercices/spy-profile-generator-plus/js/script.js
@@ -0,0 +1,89 @@
+/**
+Spy profile generator exercice
+Lucien Cusson-Fradet
+*/
+
+"use strict";
+const DATA_NAME = 'spy-game-exercice-data';
+
+let state;
+
+let objectsData;
+let religionData;
+let scientistData;
+
+let font = {
+  yoster: undefined
+}
+
+let spyProfile = {
+  realName: undefined,
+  Alias: undefined,
+  secretWeapon: undefined,
+  weaponDescription: undefined,
+  password: undefined
+};
+
+/**
+
+*/
+function preload() {
+  font.yoster = loadFont('assets/fonts/yoster.ttf');
+
+  objectsData = loadJSON('assets/data/objects.json');
+  religionData = loadJSON('assets/data/religions.json');
+  scientistData = loadJSON('assets/data/scientists.json');
+}
+
+
+/**
+
+*/
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  let loadedDataString = localStorage.getItem(DATA_NAME);
+  if (loadedDataString != null) {
+    spyProfile = JSON.parse(loadedDataString);
+    if (checkPassword()) {
+      state = new Game();
+    }
+  }
+  else {
+    state = new Initialisation();
+  }
+}
+
+
+/**
+
+*/
+function draw() {
+  background(0);
+
+  state.update();
+  state.display();
+}
+
+function keyTyped() {
+  state.keyTyped();
+}
+
+function keyPressed() {
+  if (keyCode === 105) {
+    localStorage.removeItem(DATA_NAME);
+  }
+  state.keyPressed();
+}
+
+function checkPassword() {
+  while (true) {
+    let password = prompt("Entrez votre mot de passe");
+    if (password === spyProfile.password) {
+      return true;
+    }
+    else {
+      alert('Mot de passe incorrect');
+    }
+  }
+}
