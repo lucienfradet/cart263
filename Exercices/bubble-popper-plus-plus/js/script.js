2326f8a88fe26e0c6834a3a9779a7733ffdcc671
Lucienfradet <90287745+Lucienfradet@users.noreply.github.com>
2022-02-10 15:21:19 -0500

    Ex5: stared working on the Machine learning exercice!

---
+++ Exercices/bubble-popper-plus-plus/js/script.js
@@ -0,0 +1,45 @@
+/**
+FaceBeatLegro
+Lucien Cusson-Fradet
+
+The idea is to get input from the face of the user and allow him to be some kind
+of living drumming machine
+
+Things to do:
+- Get the Face-Api working
+- Program events that would play sounds:
+  - eyes close
+  - mouth Open
+  - eyebrows raise
+  - head tilt
+    - 4 direction?
+- Import and atach sounds to the events
+
+-moreee...
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
