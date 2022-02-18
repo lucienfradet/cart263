98cbbbb5a8f712926ba424b70cc92d564b74a28c
Lucien C.Fradet <lucien.fradet@hotmail.com>
2022-02-17 22:00:13 -0500

    P1: started work on project1

---
+++ Project/Project1/js/script.js
@@ -0,0 +1,32 @@
+/**
+La grande séduction
+Lucien Cusson-Fradet
+
+Things ToDo:
+
+*/
+
+"use strict";
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
