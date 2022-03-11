256ada5568fe91db18c2a0f91a26722473be100e
Lucien C.Fradet <lucien.fradet@hotmail.com>
2022-03-11 01:34:43 -0500

    Ex6: function to select random words and wrap them in <span>

--- Exercices/raving-redactionist-plus-plus/js/script.js
+++ Exercices/raving-redactionist-plus-plus/js/script.js
@@ -7,5 +7,55 @@ author, and this description to match your project!
 */
 
 "use strict";
+function shuffle(array) {
+  let currentIndexTotal = array.length;
+  let randomIndex;
+  let placeHolder;
 
-// Code goes here
+  while (currentIndexTotal !== 0) { //keep going until the all elements have been processed
+    randomIndex = Math.floor(Math.random() * currentIndexTotal);
+    currentIndexTotal--;
+
+    //swap the elements in the array
+    placeHolder = array[currentIndexTotal];
+    array[currentIndexTotal] = array[randomIndex];
+    array[randomIndex] = placeHolder;
+  }
+
+  return array;
+}
+
+//Extrude words from the poem with REGEX
+const NUM_SECRET_WORDS = 5;
+
+let poem = $(".poem").text() //get the text in the poem class
+let wordRegex = /\b[A-zÀ-ú]{4,}\b/g //any words equal or larger than 4 letters
+let words = shuffle(poem.match(wordRegex)); //shuffle the array
+words.splice(NUM_SECRET_WORDS); //keep only the first 5 elements
+addSpan(words);
+
+function addSpan(array) {
+  $(`.poem`).each(function() { //for every element of the poem class
+    let textArray = $(this).text().split(" ");
+
+    $.each(textArray, function(j) { //for every "word" of the p text (index = j)
+      for (let i = 0; i < array.length; i++) { //compare the textArray to everyElement in the words array
+        let wordClass = `secretWord` + i; //give everyElement a different id
+
+        if (textArray[j].match(array[i])) { //replace if it matches
+          textArray[j] = `<span class="${wordClass}">` + textArray[j] + `</span>`;
+        }
+      }
+      if (textArray[j].match(`\n`)) { //place the <br> back
+        textArray[j] = `<br>`;
+      }
+    });
+    let output = textArray.join(" "); //bring everything back together and inside html
+    $(this).html(output);
+  });
+
+  for (let i = 0; i < array.length; i++) { //select one random word of every class and give it a specific id
+    let word = shuffle($(`.secretWord${i}`)).slice(0, 1);
+    word.attr('id', `word${i}`);
+  }
+}
--- Sandboxes/jQuerry/js/script.js
+++ Sandboxes/jQuerry/js/script.js
@@ -28,13 +28,57 @@ $(`.header`).css({
   "font-size": `5rem`
 });
 
-let span = $(`#example-span`)
-let spanText = span.text();
+let $span = $(`#example-span`)
+let spanText = $span.text();
 let reversedSpanText = spanText.split(``).reverse().join(``); //reverses the array
 $(`#example-span`).text(reversedSpanText);
 
 //two ways to make the text bold, with css and html
-//span.css(`font-weight`, `bolder`);
+//$span.css(`font-weight`, `bolder`);
 
 let spanHTML = $(`#example-span`).html(); //this could be inside the ${} like so: $(`#example-span`).html(`<strong>${$(`#example-span`).html();}</strong>`);
 $(`#example-span`).html(`<strong>${spanHTML}</strong>`);
+
+//attributes
+$(`#main-heading`).attr(`contentEditable`, `true`);
+
+let $link = $(`#thicc-link`);
+if ($link.attr(`href`) === `https://thi.cc`) {
+  $link.text(`BIGBUTT`);
+}
+
+//creating elements
+let $p = $(`<p>Fresh, fresh paragraph!</p>`);
+$(`#second-section`).append($p); //adds inside (at the end)
+
+let $otherP = $(`<p>Inserted paragraph!</p>`);
+$(`h2`).after($otherP); //add after (THERE IS ALSO BEFORE! lol)
+
+//removing elements
+// $(`#main-heading`).remove();
+// $(`.header`).remove();
+
+//EVENTS!!! these use the regular DOM events
+$(`#main-heading`).on(`click`, function(event) { //"event" is needed and has alot of properties and such look into in in a console.log(event)
+  $(this).append(`<p>This is added on EVERY click</p>`);
+});
+$(`#main-heading`).one(`click`, function(event) {
+  $(this).append(`<p>This is added on the FIRST click</p>`);
+});
+
+$(`p`).click(function(event) { //this is equivalent but can't use .on and .one
+  $(this).remove();
+});
+
+//remove event listeners
+let colorChange; //giving the function a name then you can specify which event function you want to remove instead of all the click events
+$(`.header`).on(`click`, colorChange = function(event) {
+  $(this).css(`color`, `red`);
+  $(`.header`).off(`click`, colorChange); //removes the EventListener after the first click
+});
+
+//INPUTS!!!
+$(`#example-button`).on(`click`, function(event) {
+  let input = $(`#example-text-input`).val(); //.val instead of value in regular JS
+  $(`#third-section`).after(`<p>${input}</p>`);
+});
