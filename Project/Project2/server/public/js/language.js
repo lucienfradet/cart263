82168a529bd274d97aa9dde3aaaf8feb5737b14b
lucienfradet <lucien.fradet@hotmail.com>
2022-04-25 12:29:58 -0400

    P2: Added language button

--- Project/Project2/server/public/js/language.js
+++ Project/Project2/server/public/js/language.js
@@ -1,11 +1,9 @@
 //Language selection function using the MultiLanguage library
 
-$("#french_button").on(`click`, () => {
-  setLanguage('fr');
-  //console.log(getLanguage());
-});
-
-$("#english_button").on(`click`, () => {
-  setLanguage('en');
-  //console.log(getLanguage());
-});
+function languageSwitch() {
+  if (language === 'fr') {
+    setLanguage('en');
+  } else if (language === 'en') {
+    setLanguage('fr');
+  }
+}
